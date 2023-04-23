84c629282887779941bd36834d8d9635c2e703cf
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-04-22 21:56:58 -0230

    markup, style, and simple functions

---
+++ function.js
@@ -0,0 +1,35 @@
+const submit_button = document.querySelector(".button");
+submit_button.onclick = () => 
+{
+    e.preventDefault();
+    // all input data receive these variables
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const pass  = document.getElementById("pass").value;
+    const cpass = document.getElementById("cpass").value;
+
+    // storing this data in web browser store
+    localStorage.setItem('FirstName', fname);
+    localStorage.setItem('LastName', lname);
+    localStorage.setItem('Email', email);
+    localStorage.setItem('Password', pass);
+    localStorage.setItem('Cpassword', cpass);
+
+    // setting conditions
+    if(fname == "" && lname == "" && email == "" && pass == "" && cpass == "")
+     {
+        swal("Opps..!", "Input field must be filled", "error");
+     }
+     else
+     {
+        if(pass !== cpass) 
+        {
+            swal("Opps..!", "The passwords you entered are not matching", "error");
+        }
+        else
+        {
+            swal("Registration successful!", "You created a new account!", "success");
+        }
+     }
+}
